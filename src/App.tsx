438252299209f72564d20bcbbc8a3726382ee0d7
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-20 16:08:10 -0300

    feat: add first layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Footer } from './components/footer'
+import { Header } from './components/header'
 
 function App() {
 
 
   return (
     <>
+    <Header/>
+    <main className='flex-1'>
+    </main>
+    <Footer />
     </>
   )
 }
