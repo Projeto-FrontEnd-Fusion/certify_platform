6f95adc070dbd5223314d598d696268badec4296
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-21 06:46:58 -0300

    feat: create config for images used in section RequestCertificade

---
+++ src/config/RequestCertificadeImages.ts
@@ -0,0 +1,9 @@
+import SmilingGirl from "@/assets/SmilingGirl.webp";
+import SmilingKid from "@/assets/SmilingKid.webp";
+import SurprisedMan from "@/assets/SurprisedMan.webp";
+
+export const Images = [
+  { id: 1, src: SmilingGirl, alt: "Imagem de uma mulher sorridente" },
+  { id: 2, src: SmilingKid, alt: "Imagem de uma criança sorridente" },
+  { id: 3, src: SurprisedMan, alt: "Imagem de um homem surpreso" },
+];
