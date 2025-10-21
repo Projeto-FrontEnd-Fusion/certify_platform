549a2df0cafcb9410b678587b0c524b5cd30129c
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-21 05:44:24 -0300

    feat: create type for Message component

---
+++ src/types/Message.ts
@@ -0,0 +1,13 @@
+import { errors } from "@/config/ErrorsMessages";
+
+interface SuccessMessage {
+  status: "sucess";
+  messageError?: never;
+}
+
+interface ErrorMessage {
+  status: "error";
+  messageError: keyof typeof errors;
+}
+
+export type MessageI = SuccessMessage | ErrorMessage;
