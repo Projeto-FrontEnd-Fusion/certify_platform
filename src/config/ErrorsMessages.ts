7592af13829c25ad137e48e03d4489ca68288247
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-21 05:44:45 -0300

    feat: create config for errors messages

---
+++ src/config/ErrorsMessages.ts
@@ -0,0 +1,6 @@
+export const errors = {
+  email:
+    "Verifique se o e-mail informado está correto e tente novamente. Se o problema persistir, entre em contato com o suporte da CertiFy.",
+  name:
+    "Verifique se o nome informado está correto e tente novamente. Se o problema persistir, entre em contato com o suporte da CertiFy.",
+};
