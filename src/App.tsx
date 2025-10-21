45b4f9b7f855c06cdd72ad0780505fae118f7f3d
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-20 23:59:55 -0300

    feat: use React Router Dom to create Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import './App.css'
-import { Footer } from './components/footer'
-import { Header } from './components/header'
+import "./App.css";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+import { WhatDoYouWantToDo } from "./components/WhatDoYouWantToDo";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-    <Header/>
-    <main className='flex-1'>
-    </main>
-    <Footer />
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route Component={WhatDoYouWantToDo} path="/" />
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
