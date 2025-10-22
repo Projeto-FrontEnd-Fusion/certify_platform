79e501b3de951ed128c46654ea8b0a503088462c
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-22 18:16:43 -0300

    feat: update name and validation

---
+++ src/schemas/requestCertificadeLogin.ts
@@ -0,0 +1,12 @@
+import z from "zod/v3";
+
+export const RequestCertificadeLoginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "O e-mail é obrigatório")
+    .email("Digite um e-mail válido"),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" })
+    .max(100, { message: "A senha deve ter no máximo 100 caracteres" }),
+});
