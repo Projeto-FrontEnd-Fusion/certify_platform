28022c4fc0465b5b8d31cc9569a3d97bc6717a31
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-22 18:14:02 -0300

    refactor: change the home path / and delete WhatDoYouWantToDo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
-import { RequestCertificate } from "./components/RequestCertificate";
-import { WhatDoYouWantToDo } from "./components/WhatDoYouWantToDo";
+import { RequestCertificateLogin } from "./components/RequestCertificateLogin";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,8 +11,7 @@ function App() {
       <Header />
       <main className="flex-1">
         <Routes>
-          <Route Component={WhatDoYouWantToDo} path="/" />
-          <Route Component={RequestCertificate} path="/requestCertificate" />
+          <Route Component={RequestCertificateLogin} path="/" />
         </Routes>
       </main>
       <Footer />
