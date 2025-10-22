28c1d922a6fbff31b4254c91e8a2650a6fb46a18
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-22 18:15:09 -0300

    styles: add new colors for fonts and logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ export const Header = () => {
   return (
     <header className="flex justify-between px-4 py-5 items-center">
       <div className="flex gap-1">
-        <Logo width={32} height={32} role="img" aria-label="Logo Certify" />
-        <h1 className="font-lato text-[1.25rem] font-black">
+        <Logo width={32} height={32} role="img" aria-label="Logo Certify" className="text-[#3925DD]"/>
+        <h1 className="font-lato text-[1.25rem] font-black text-[#3925DD]">
           Certi<span className="font-light">fy</span>
         </h1>
       </div>
 
-      <SlMenu className="text-2xl cursor-pointer" />
+      <SlMenu className="text-2xl cursor-pointer text-[#3925DD]" />
     </header>
   );
 };
