259a8409f8dda339232298e3100292d6c237c247
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-23 00:58:13 -0300

    feat: create config file for menu

---
+++ src/config/Menu.ts
@@ -0,0 +1,27 @@
+import { LiaUserEditSolid } from "react-icons/lia";
+import { LuQrCode } from "react-icons/lu";
+import { PiCreditCard } from "react-icons/pi";
+import { RxExit } from "react-icons/rx";
+
+export const menuItems = [
+  {
+    Icon: LiaUserEditSolid,
+    label: "Meus Dados",
+    path: "/",
+  },
+  {
+    Icon: PiCreditCard,
+    label: "Meus Certificados",
+    path: "/",
+  },
+  {
+    Icon: LuQrCode,
+    label: "Validação",
+    path: "/",
+  },
+  {
+    Icon: RxExit,
+    label: "Sair",
+    path: "/",
+  },
+];
