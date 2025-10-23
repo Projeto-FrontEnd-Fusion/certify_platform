4a871684a29de2f78d3dccb35b0703b2baa7bc8b
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-22 22:21:06 -0300

    feat: create new schema for RequestCertificadeRegister component validation

---
+++ src/schemas/requestCertificadeRegister.ts
@@ -0,0 +1,16 @@
+import z from "zod/v3";
+
+export const RequestCertificadeRegisterSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "O nome deve ter pelo menos 3 caracteres" })
+    .max(60, { message: "O nome deve ter no máximo 60 caracteres" }),
+  email: z
+    .string()
+    .min(1, "O e-mail é obrigatório")
+    .email("Digite um e-mail válido"),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" })
+    .max(100, { message: "A senha deve ter no máximo 100 caracteres" }),
+});
