7e8a7bb6314b6087d917459f18951f2a1c2e3e4b
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-28 01:48:59 -0300

    refactor: extract component logic, add loading state and disabled buttons for better UX

--- src/components/DownloadCertificate.tsx
+++ src/components/DownloadCertificate.tsx
@@ -1,41 +1,12 @@
 import { LuDownload } from "react-icons/lu";
 import { useRef } from "react";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas-pro";
+import { useDownload } from "@/hooks/useDownload";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const DownloadCertificate = () => {
   const printRef = useRef<HTMLDivElement>(null);
-  const linkRef = useRef<HTMLAnchorElement>(null);
 
-  const handleDownloadPdf = async () => {
-    if (!printRef.current) return;
-
-    const canvas = await html2canvas(printRef.current);
-    const imgData = canvas.toDataURL("image/png");
-
-    const pdf = new jsPDF("p", "mm", "a4");
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-    pdf.save("DevInsigths");
-  };
-
-  const handleDownloadPng = async () => {
-    if (!printRef.current || !linkRef.current) return;
-
-    const canvas = await html2canvas(printRef.current);
-
-    canvas.toBlob((blob) => {
-      if (!blob || !linkRef.current) return;
-
-      const url = URL.createObjectURL(blob);
-      linkRef.current.href = url;
-      linkRef.current.download = "DevInsigths";
-      linkRef.current.click();
-      URL.revokeObjectURL(url);
-    }, "image/png");
-  };
+  const { loadingMethod, handleDownload } = useDownload();
 
   return (
     <section className="bg-[#F2F2F9] h-full font-inter flex flex-col items-center pt-13 gap-4">
@@ -58,15 +29,28 @@ export const DownloadCertificate = () => {
       </div>
 
       <div className="flex gap-2">
-        {["PDF", "PNG"].map((methods) => (
-          <a
-            ref={methods === "PNG" ? linkRef : null}
-            onClick={methods === "PDF" ? handleDownloadPdf : handleDownloadPng}
+        {(["PDF", "PNG"] as const).map((methods) => (
+          <button
             key={methods}
+            type="button"
+            disabled={loadingMethod === methods}
+            onClick={() =>
+              handleDownload({
+                element: printRef.current,
+                fileName: "DevInsigths",
+                method: methods,
+              })
+            }
             className="flex items-center gap-2 py-2 bg-[#3925DD] w-26 justify-center text-white text-xl rounded cursor-pointer duration-300 transition-transform hover:scale-105"
           >
-            {methods} <LuDownload size={20} />
-          </a>
+            {loadingMethod === methods ? (
+              <AiOutlineLoading3Quarters size={20} className="animate-spin" />
+            ) : (
+              <>
+                {`${methods}`} <LuDownload size={20} />
+              </>
+            )}
+          </button>
         ))}
       </div>
     </section>
