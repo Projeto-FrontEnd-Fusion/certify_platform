32d9b40f362b1f0f4460003d304cc2f9373db292
dinho <claudiosilva.one@gmail.com>
2025-10-29 10:29:02 -0300

    feat: create-zustand-store-for-user-auth-data

---
+++ src/stores/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface authPayload {
+  _id: string;
+  fullname?: string;
+  role: "user" | "admin";
+  email: string;
+  created_at?: Date,
+  updated_at?: Date 
+}
+
+interface authStoreProps {
+  auth: authPayload | null;
+  setAuthLogin: (auth: authPayload) => void;
+  authLogout: () => void;
+}
+
+export const authStoreData = create<authStoreProps>()(
+  persist(
+    (set) => ({
+      auth: null,
+      setAuthLogin: (auth) =>
+        set({
+          auth: auth,
+        }),
+      authLogout: () => set({ auth: null }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
