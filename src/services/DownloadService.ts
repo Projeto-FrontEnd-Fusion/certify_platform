2da39877f70b84f79e89078416028106b3cad023
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-31 01:06:59 -0300

    feat: show alert when element not found and use JPEG for smaller certificate size

--- src/services/DownloadService.ts
+++ src/services/DownloadService.ts
@@ -4,21 +4,21 @@ import jsPDF from "jspdf";
 
 export class DownloadService {
   async generatePDF({ element, fileName }: IDownload) {
-    if (!element) return;
+    if (!element) return alert("Elemento não encontrado!");
 
-    const canvas = await html2canvas(element, { scale: 4 });
-    const imgData = canvas.toDataURL("image/png");
+    const canvas = await html2canvas(element, { scale: 10 });
+    const imgData = canvas.toDataURL("image/jpeg");
     const pdf = new jsPDF("l", "mm", "a4");
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.addImage(imgData, "JPEG", 0, 0, pdfWidth, pdfHeight);
     pdf.save(`${fileName}.pdf`);
   }
 
   /* canvas.toBlob uses a callback and is asynchronous,
      so we wrap it in a Promise to use await and handle errors properly */
   async generatePNG({ element, fileName }: IDownload) {
-    if (!element) return;
+    if (!element) return alert("Elemento não encontrado!");
     const canvas = await html2canvas(element, { scale: 4 });
     await new Promise<void>((resolve, reject) => {
       canvas.toBlob((blob) => {
