e3bbbadaa4c7452ac2ae959c50466162487b6978
Davi Santanna <davicalvalcanti@gmail.com>
2025-11-04 04:22:40 -0300

    feat: create CertificateApiInstance with baseURL for CertificateService

--- src/api/implements.ts
+++ src/api/implements.ts
@@ -1,7 +1,13 @@
+import { CertificateApiInstance } from "./Acess/CertificateHTTPclient";
+import { CertificateService } from "./Acess/CertificateService";
 import { AuthApiInstance } from "./Auth/AuthHTTPclient";
 import { AuthService } from "./Auth/AuthService";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
-const authApiIntance = AuthApiInstance.getInstance(API_URL)
-export const authServiceInstance = new AuthService(authApiIntance)
+const authApiIntance = AuthApiInstance.getInstance(API_URL);
+export const authServiceInstance = new AuthService(authApiIntance);
+
+const acessApiInstance = CertificateApiInstance.getInstance(API_URL);
+
+export const acessServiceInstance = new CertificateService(acessApiInstance);
