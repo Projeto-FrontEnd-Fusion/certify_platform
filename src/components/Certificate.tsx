9a16d4d9ac3d15e139695370fd3476b6120006f6
Davi Santanna <davicalvalcanti@gmail.com>
2025-11-04 04:25:29 -0300

    refactor: integrate CertificateStore and dynamic access data

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -2,70 +2,70 @@ import { LuCalendarCheck2, LuCalendarFold, LuHourglass } from "react-icons/lu";
 import BackgroundImage from "@/assets/BackgroundCertificate.svg";
 import MedalCertificate from "@/assets/MedalCertificate.svg";
 import { useAuthStoreData } from "@/stores/useAuthStore";
+import { useCertificateStoreData } from "@/stores/useCertificateStore";
+import { formatDate } from "@/utils/FormatDate";
 
 export const Certificate = () => {
-  const {auth} = useAuthStoreData()
+  const { auth } = useAuthStoreData();
+  const { Access } = useCertificateStoreData();
+
+  const decriptionSplit =
+    Access.description?.split(Access.event_name || "") || [];
+
   return (
-     <div
-        style={{ backgroundImage: `url(${BackgroundImage})` }}
-        className={`w-[297mm] h-[210mm] bg-cover bg-center px-16 flex 
-          flex-col py-14 relative scale-[0.3] md:scale-[0.6] lg:scale-[0.8] 
-          xl:scale-100 origin-top`}
-      >
-        <img
-          src={MedalCertificate}
-          alt="Medalha de certificação"
-          className="w-52 absolute right-3 top-10"
-        />
-        <h2 className="text-8xl text-[#F79021] font-judson mb-12 z-10">
-          Certificado
-        </h2>
+    <div
+      style={{ backgroundImage: `url(${BackgroundImage})` }}
+      className={`w-[297mm] h-[210mm] bg-cover bg-center px-16 flex 
+        flex-col py-14 relative scale-[0.3] md:scale-[0.6] lg:scale-[0.8] 
+        xl:scale-100 origin-top`}
+    >
+      <img
+        src={MedalCertificate}
+        alt="Medalha de certificação"
+        className="w-52 absolute right-3 top-10"
+      />
 
-        <p className="text-[26px] text-white z-10">
-          A COMUNIDADE FRONTEND FUSION CERTIFICA QUE:
-        </p>
+      <h2 className="text-8xl text-[#F79021] font-judson mb-12 z-10">
+        Certificado
+      </h2>
 
-        <h3 className="text-6xl text-[#32E399] mt-3.5 mb-8 z-10">
-          {auth?.fullname}
-        </h3>
+      <p className="text-[26px] text-white z-10 uppercase">
+        a {Access.institution_name} que:
+      </p>
 
-        <p className="font-extralight text-white text-[26px] -tracking-wider mb-4 z-10">
-          Participou da <span className="font-bold">Imersão Dev Insights</span>,
-          realizada nos dias 5, 6 e 7 de novembro, um evento voltado ao
-          aprendizado, conexão e desenvolvimento em gestão de produtos e
-          metodologias ágeis.
-        </p>
+      <h3 className="text-6xl text-[#32E399] mt-3.5 mb-8 z-10">
+        {Access.participant_name || auth?.fullname}
+      </h3>
 
-        <p className="font-extralight text-white text-[26px] -tracking-wider z-10">
-          Durante esta imersão, o(a) participante teve contato com conteúdos
-          ministrados por profissionais experientes, desenvolvendo habilidades
-          essenciais para atuar em ambientes ágeis e projetos de produtos
-          digitais.
-        </p>
+      <p className="font-extralight text-white text-[26px] -tracking-wider mb-4 z-10 break-words">
+        {decriptionSplit[0]}
+        <strong className="font-bold">{Access.event_name}</strong>
+        {decriptionSplit[1]}
+      </p>
 
-        <ul className="flex gap-14 mt-12 z-10">
-          <li className="flex text-2xl text-white gap-2">
-            <LuHourglass color="#32E399" strokeWidth={1} size={56} />
-            <div className="flex flex-col">
-              <span className="font-light">Carga Horária</span>
-              <span className="font-bold">09 Horas</span>
-            </div>
-          </li>
-          <li className="flex text-2xl text-white gap-2">
-            <LuCalendarFold color="#32E399" strokeWidth={1} size={56} />
-            <div className="flex flex-col">
-              <span className="font-light">Início</span>
-              <span className="font-bold">05/11/2025</span>
-            </div>
-          </li>
-          <li className="flex text-2xl text-white gap-2">
-            <LuCalendarCheck2 color="#32E399" strokeWidth={1} size={56} />
-            <div className="flex flex-col">
-              <span className="font-light">Conclusão</span>
-              <span className="font-bold">07/11/2025</span>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <ul className="flex gap-14 mt-12 z-10">
+        <li className="flex text-2xl text-white gap-2">
+          <LuHourglass color="#32E399" strokeWidth={1} size={56} />
+          <div className="flex flex-col">
+            <span className="font-light">Carga Horária</span>
+            <span className="font-bold">{Access.workload || 0} Horas</span>
+          </div>
+        </li>
+        <li className="flex text-2xl text-white gap-2">
+          <LuCalendarFold color="#32E399" strokeWidth={1} size={56} />
+          <div className="flex flex-col">
+            <span className="font-light">Início</span>
+            <span className="font-bold">{formatDate(Access.event_start)}</span>
+          </div>
+        </li>
+        <li className="flex text-2xl text-white gap-2">
+          <LuCalendarCheck2 color="#32E399" strokeWidth={1} size={56} />
+          <div className="flex flex-col">
+            <span className="font-light">Conclusão</span>
+            <span className="font-bold">{formatDate(Access.event_end)}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
--- src/components/DummyCertificate.tsx
+++ src/components/DummyCertificate.tsx
@@ -2,12 +2,19 @@ import { type RefObject } from "react";
 import { LuHourglass, LuCalendarFold, LuCalendarCheck2 } from "react-icons/lu";
 import MedalCertificate from "@/assets/MedalCertificate.svg";
 import BackgroundImage from "@/assets/BackgroundCertificate.svg";
+import { formatDate } from "@/utils/FormatDate";
+import { useAuthStoreData } from "@/stores/useAuthStore";
+import { useCertificateStoreData } from "@/stores/useCertificateStore";
 
 export const DummyCertificade = ({
   printRef,
 }: {
   printRef: RefObject<HTMLDivElement | null>;
 }) => {
+  const { auth } = useAuthStoreData();
+  const { Access } = useCertificateStoreData();
+  const decriptionSplit =
+    Access.description?.split(Access.event_name || "") || [];
   return (
     <>
       <div
@@ -21,53 +28,47 @@ export const DummyCertificade = ({
           alt="Medalha de certificação"
           className="w-52 absolute right-3 top-10"
         />
+
         <h2 className="text-8xl text-[#F79021] font-judson mb-12 z-10">
           Certificado
         </h2>
         {/* Limite de 40 caracteres */}
-        <p className=" text-[26px] text-white z-10">
-          A COMUNIDADE FRONTEND FUSION CERTIFICA QUE:
+        <p className="text-[26px] text-white z-10 uppercase">
+          a {Access.institution_name} que:
         </p>
         {/* Limite de 40 caracteres */}
         <h3 className="text-6xl text-[#32E399] mt-3.5 mb-8 z-10">
-          Gustavo Guanabara da Silva
+          {Access.participant_name || auth?.fullname}
         </h3>
-        {/* Limite de 260 caracteres */}
-        <p className="font-extralight text-white  text-[26px] -tracking-wider mb-4 z-10">
-          Participou da <span className="font-bold">Imersão Dev Insights</span>,
-          realizada nos dias 5, 6 e 7 de novembro, um evento voltado ao
-          aprendizado, conexão e desenvolvimento em gestão de produtos e
-          metodologias ágeis.
-        </p>
-        {/* Limite de 260 caracteres */}
-        <p className="font-extralight text-white  text-[26px]  -tracking-wider z-10">
-          Durante esta imersão, o(a) participante teve contato com conteúdos
-          ministrados por profissionais experientes, desenvolvendo habilidades
-          essenciais para atuar em ambientes ágeis e projetos de produtos
-          digitais.
+        {/* Limite de 500 caracteres */}
+        <p className="font-extralight text-white text-[26px] -tracking-wider mb-4 z-10">
+          {decriptionSplit[0]}
+          <strong className="font-bold">{Access.event_name}</strong>
+          {decriptionSplit[1]}
         </p>
 
         <ul className="flex gap-14 mt-12 z-10">
           <li className="flex text-2xl text-white gap-2">
             <LuHourglass color="#32E399" strokeWidth={1} size={56} />
-
             <div className="flex flex-col">
-              <span className="font-light ">Carga Horária</span>
-              <span className="font-bold">09 Horas</span>
+              <span className="font-light">Carga Horária</span>
+              <span className="font-bold">{Access.workload || 0} Horas</span>
             </div>
           </li>
           <li className="flex text-2xl text-white gap-2">
             <LuCalendarFold color="#32E399" strokeWidth={1} size={56} />
             <div className="flex flex-col">
-              <span className="font-light ">Início</span>
-              <span className="font-bold">05/11/2025</span>
+              <span className="font-light">Início</span>
+              <span className="font-bold">
+                {formatDate(Access.event_start)}
+              </span>
             </div>
           </li>
           <li className="flex text-2xl text-white gap-2">
             <LuCalendarCheck2 color="#32E399" strokeWidth={1} size={56} />
             <div className="flex flex-col">
-              <span className="font-light ">Conclusão</span>
-              <span className="font-bold">05/11/2025</span>
+              <span className="font-light">Conclusão</span>
+              <span className="font-bold">{formatDate(Access.event_end)}</span>
             </div>
           </li>
         </ul>
