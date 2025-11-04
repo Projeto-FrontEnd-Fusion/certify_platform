096dd878cfbf289b90daf21fcdae212d5337ad23
Davi Santanna <davicalvalcanti@gmail.com>
2025-11-04 04:20:31 -0300

    feat: create CertificateApiInstance to manage Axios instance

---
+++ src/api/Acess/CertificateHTTPclient.ts
@@ -0,0 +1,31 @@
+import axios, { type AxiosInstance } from "axios";
+
+export class CertificateApiInstance {
+  private static instance: AxiosInstance | null = null;
+
+  private constructor() {}
+
+  public static createInstance(baseURL: string): AxiosInstance {
+    const setup = axios.create({
+      baseURL,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      timeout: 10000,
+    });
+    return setup;
+  }
+
+  public static getInstance(API_URL?: string): AxiosInstance {
+    if (!API_URL) {
+      throw new Error("API_URL é obrigatória para a primeira inicialização");
+    }
+
+    if (!this.instance) {
+      this.instance = this.createInstance(API_URL);
+    }
+
+    return this.instance;
+  }
+}
