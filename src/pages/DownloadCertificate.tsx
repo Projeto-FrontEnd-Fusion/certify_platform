72ee2e655a597332cfe136a548c0f79be782d82c
Davi Santanna <davicalvalcanti@gmail.com>
2025-11-06 14:17:59 -0300

    styles: change max width and text size for better responsiveness a

--- src/pages/DownloadCertificate.tsx
+++ src/pages/DownloadCertificate.tsx
@@ -10,11 +10,11 @@ export const DownloadCertificate = () => {
     return <Navigate replace to={"/meus-certificados"} />;
   return (
     <section className="bg-[#F2F2F9] h-full font-inter flex flex-col items-center pt-13 gap-4">
-      <div className="space-y-2 text-center">
-        <h2 className="text-xl font-semibold text-[#1A1551]">
+      <div className="space-y-2 text-center max-w-[20rem] md:max-w-[30rem]">
+        <h2 className="text-xl font-semibold text-[#1A1551] md:text-3xl">
           Seu certificado está pronto!!
         </h2>
-        <p className="text-[#1a1551cc]">
+        <p className="text-[#1a1551cc] md:text-lg">
           Parabéns pela conquista! Agora você pode baixar e compartilhar seu
           reconhecimento.
         </p>
