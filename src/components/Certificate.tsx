aaf14e0d23ff6fcf94d96c2679202392ff341c00
Davi Santanna <davicalvalcanti@gmail.com>
2025-11-07 03:25:34 -0300

    styles: add new breakpoints for better responsiveness and uppercase

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -16,7 +16,7 @@ export const Certificate = () => {
     <div
       style={{ backgroundImage: `url(${BackgroundImage})` }}
       className={`w-[297mm] h-[210mm] bg-cover bg-center px-16 flex 
-        flex-col py-14 relative scale-[0.3] md:scale-[0.6] lg:scale-[0.8] 
+        flex-col py-14 relative scale-[0.25] min-[370px]:scale-[0.3] min-[500px]:scale-[0.4] sm:scale-[0.5] md:scale-[0.6] lg:scale-[0.8] 
         xl:scale-100 origin-top`}
     >
       <img
@@ -25,7 +25,7 @@ export const Certificate = () => {
         className="w-52 absolute right-3 top-10"
       />
 
-      <h2 className="text-8xl text-[#F79021] font-judson mb-12 z-10">
+      <h2 className="text-8xl text-[#F79021] font-judson mb-12 z-10 uppercase">
         Certificado
       </h2>
 
@@ -33,7 +33,7 @@ export const Certificate = () => {
         a {Access.institution_name} que:
       </p>
 
-      <h3 className="text-6xl text-[#32E399] mt-3.5 mb-8 z-10">
+      <h3 className="text-6xl text-[#32E399] mt-3.5 mb-8 z-10 uppercase">
         {Access.participant_name || auth?.fullname}
       </h3>
 
@@ -47,21 +47,21 @@ export const Certificate = () => {
         <li className="flex text-2xl text-white gap-2">
           <LuHourglass color="#32E399" strokeWidth={1} size={56} />
           <div className="flex flex-col">
-            <span className="font-light">Carga Horária</span>
+            <span className="font-light uppercase">Carga Horária</span>
             <span className="font-bold">{Access.workload || 0} Horas</span>
           </div>
         </li>
         <li className="flex text-2xl text-white gap-2">
           <LuCalendarFold color="#32E399" strokeWidth={1} size={56} />
           <div className="flex flex-col">
-            <span className="font-light">Início</span>
+            <span className="font-light uppercase">Início</span>
             <span className="font-bold">{formatDate(Access.event_start)}</span>
           </div>
         </li>
         <li className="flex text-2xl text-white gap-2">
           <LuCalendarCheck2 color="#32E399" strokeWidth={1} size={56} />
           <div className="flex flex-col">
-            <span className="font-light">Conclusão</span>
+            <span className="font-light uppercase">Conclusão</span>
             <span className="font-bold">{formatDate(Access.event_end)}</span>
           </div>
         </li>
