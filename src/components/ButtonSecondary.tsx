5bf08299b025e18ef3befe8fe9da9d388f19a0cd
CaioMiranda12 <caioclm04@gmail.com>
2026-04-02 12:53:56 -0300

    fix: update padding-x of button secondary

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -9,7 +9,7 @@ export function SecondaryButton({ children, isDisabled = false }: SecondaryButto
   return (
     <button
       disabled={isDisabled}
-      className="py-5 px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
+      className="py-5 px-5 lg:px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
       hover:bg-primary-blue-100 hover:text-primary-blue-300 transition-colors duration-300 
       active:text-primary-blue-500
       disabled:border-3 disabled:border-primary-gray-100 disabled:text-primary-gray-300 disabled:cursor-not-allowed disabled:hover:bg-transparent
