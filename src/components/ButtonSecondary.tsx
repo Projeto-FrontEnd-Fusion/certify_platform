1b753840a56c81bc2267c29f0b67a2e96a50b412
CaioMiranda12 <caioclm04@gmail.com>
2026-04-02 12:12:14 -0300

    feat: create secondary button component

---
+++ src/components/ButtonSecondary.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+
+interface SecondaryButtonProps {
+  children: ReactNode;
+  isDisabled?: boolean;
+}
+
+export function SecondaryButton({ children, isDisabled = false }: SecondaryButtonProps) {
+  return (
+    <button
+      disabled={isDisabled}
+      className="py-5 px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
+      hover:bg-primary-blue-100 hover:text-primary-blue-300 transition-colors duration-300 
+      active:text-primary-blue-500
+      disabled:border-3 disabled:border-primary-gray-100 disabled:text-primary-gray-300 disabled:cursor-not-allowed disabled:hover:bg-transparent
+    ">
+      {children}
+    </button>
+  )
+}
