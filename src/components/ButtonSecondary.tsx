3acf0e2a0e0caf56da900a0711563973019150a5
CaioMiranda12 <caioclm04@gmail.com>
2026-04-19 17:27:57 -0300

    feat: add onclick and isActive prop on secondary button

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -3,17 +3,25 @@ import type { ReactNode } from "react";
 interface SecondaryButtonProps {
   children: ReactNode;
   isDisabled?: boolean;
+  onClick?: () => void;
+  isActive?: boolean;
 }
 
-export function SecondaryButton({ children, isDisabled = false }: SecondaryButtonProps) {
+export function SecondaryButton({ children, isDisabled = false, onClick, isActive }: SecondaryButtonProps) {
+  const activeStyle = isActive
+    ? "bg-primary-blue-100 text-primary-blue-500"
+    : "";
+
   return (
     <button
       disabled={isDisabled}
-      className="py-5 px-5 lg:px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
+      onClick={onClick}
+      className={`w-full py-5 px-5 lg:px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
       hover:bg-primary-blue-100 hover:text-primary-blue-300 transition-colors duration-300 
       active:text-primary-blue-500
       disabled:border-3 disabled:border-primary-gray-100 disabled:text-primary-gray-300 disabled:cursor-not-allowed disabled:hover:bg-transparent
-    ">
+      ${activeStyle}
+      `}>
       {children}
     </button>
   )
