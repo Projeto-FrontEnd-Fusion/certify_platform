80b9ffd9d060b3e39f2c4150fe3c22c62dacc576
CaioMiranda12 <caioclm04@gmail.com>
2026-05-26 21:45:00 -0300

    style: add h-20 to button secondary component

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -16,7 +16,7 @@ export function SecondaryButton({ children, isDisabled = false, onClick, isActiv
     <button
       disabled={isDisabled}
       onClick={onClick}
-      className={`w-full py-5 px-5 lg:px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none
+      className={`w-full py-5 px-5 lg:px-12 border-2 border-primary-blue-base text-primary-blue-base font-bold text-[27px] rounded-[20px] cursor-pointer outline-none h-20 flex items-center justify-center
       hover:bg-primary-blue-100 hover:text-primary-blue-300 transition-colors duration-300 
       active:text-primary-blue-500
       disabled:border-3 disabled:border-primary-gray-100 disabled:text-primary-gray-300 disabled:cursor-not-allowed disabled:hover:bg-transparent
